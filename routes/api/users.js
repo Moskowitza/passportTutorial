cc81ad9761038c8409cfe19b44c9747349222506
moskowitza <moskowitzaaron@mac.com>
2018-10-11 14:31:00 -0400

    saving

--- routes/api/users.js
+++ routes/api/users.js
@@ -69,15 +69,15 @@ router.post('/login', auth.optional, (req, res, next) => {
 //GET current route (required, only authenticated users have access)
 router.get('/current', auth.required, (req, res, next) => {
     const { payload: { id } } = req;
-  
+
     return Users.findById(id)
-      .then((user) => {
-        if(!user) {
-          return res.sendStatus(400);
-        }
-  
-        return res.json({ user: user.toAuthJSON() });
-      });
-  });
-  
-  module.exports = router;
+        .then((user) => {
+            if (!user) {
+                return res.sendStatus(400);
+            }
+
+            return res.json({ user: user.toAuthJSON() });
+        });
+});
+
+module.exports = router;
